9b3b5fff43a934aa540c20fa2365989dcda46d6d
Tanuj Choudhary <tanujchoudhary001@gmail.com>
2023-03-15 23:01:48 +0530

    Added all inputs to form

--- form-using-state/src/Form.jsx
+++ form-using-state/src/Form.jsx
@@ -1,12 +1,16 @@
-import { Component } from "react";
-import "./form.css";
+import { Component } from 'react';
+import './form.css';
 
 export default class Form extends Component {
   state = {
-    name: "",
-    email: "",
-    password: "",
-    number: "",
+    name: '',
+    email: '',
+    password: '',
+    number: '',
+    vehicles: {},
+    gender: '',
+    color: '',
+    url: '',
   };
 
   onNameChange = (e) => {
@@ -25,8 +29,54 @@ export default class Form extends Component {
     this.setState({ number: e.target.value });
   };
 
+  onCheckBoxChange = (e) => {
+    const { checked, value } = e.target;
+
+    if (!checked) {
+      this.setState((prevState) => {
+        const newState = { ...prevState };
+        delete newState.vehicles[value];
+        return newState;
+      });
+    } else {
+      this.setState((prevState) => {
+        const newState = { ...prevState };
+        newState.vehicles[value] = 1;
+        return newState;
+      });
+    }
+  };
+
+  onRadioButtonChanged = (e) => {
+    this.setState({ gender: e.target.value });
+  };
+
+  onColorChange = (e) => {
+    this.setState({ color: e.target.value });
+  };
+
+  onURLChange = (e) => {
+    this.setState({ url: e.target.value });
+  };
+
+  onDateChange = (e) => {
+    this.setState({
+      date: e.target.value,
+    });
+  };
+
   render() {
-    const { name, email, password, number } = this.state;
+    const {
+      name,
+      email,
+      password,
+      number,
+      vehicles,
+      gender,
+      color,
+      url,
+      date,
+    } = this.state;
 
     return (
       <form>
@@ -51,6 +101,98 @@ export default class Form extends Component {
         <label htmlFor="number">Number:</label>
         <input onChange={this.onNumberChange} type="number" />
         <span>{number}</span>
+
+        <br />
+
+        <input
+          onChange={this.onCheckBoxChange}
+          type="checkbox"
+          name="vehicle"
+          id="car"
+          value="car"
+        />
+        <label htmlFor="car">Car</label>
+
+        <input
+          onChange={this.onCheckBoxChange}
+          type="checkbox"
+          name="vehicle"
+          id="bike"
+          value="bike"
+        />
+        <label htmlFor="bike">Bike</label>
+
+        <input
+          onChange={this.onCheckBoxChange}
+          type="checkbox"
+          name="vehicle"
+          id="truck"
+          value="truck"
+        />
+        <label htmlFor="truck">Truck</label>
+
+        <div>
+          Checkbox Selected:
+          {Object.keys(vehicles).map((vehicle) => (
+            <span className="vehicle" key={vehicle}>
+              {vehicle}
+            </span>
+          ))}
+        </div>
+
+        <br />
+
+        <input
+          onChange={this.onRadioButtonChanged}
+          type="radio"
+          name="gender"
+          id="male"
+          value="male"
+        />
+        <label htmlFor="male">Male:</label>
+
+        <input
+          onChange={this.onRadioButtonChanged}
+          type="radio"
+          name="gender"
+          id="female"
+          value="female"
+        />
+        <label htmlFor="female">Female:</label>
+
+        <input
+          onChange={this.onRadioButtonChanged}
+          type="radio"
+          name="gender"
+          id="other"
+          value="other"
+        />
+        <label htmlFor="other">Other:</label>
+
+        <div>Radio Button Selected: {gender}</div>
+
+        <br />
+
+        <label htmlFor="color">Choose color:</label>
+        <input
+          onChange={this.onColorChange}
+          type="color"
+          name="color"
+          id="color"
+        />
+        <span>{color}</span>
+
+        <br />
+
+        <label htmlFor="URL">URL:</label>
+        <input onChange={this.onURLChange} type="url" />
+        <span>{url}</span>
+
+        <br />
+
+        <label htmlFor="date">Date:</label>
+        <input onChange={this.onDateChange} type="date" />
+        <span>{date}</span>
       </form>
     );
   }
